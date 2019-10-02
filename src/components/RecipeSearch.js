834a4aff15805ce7e89353db9ae8914e03110993
Khushi <[rupinder.ace87@gmail.com]>
2019-10-02 00:06:40 -0500

    cookwithme

--- src/components/RecipeSearch.js
+++ src/components/RecipeSearch.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class RecipeList extends Component {
   render() {
@@ -9,12 +9,12 @@ export default class RecipeList extends Component {
           <div className="row">
             <div className="col-10 mx-auto col-md-8 mt-5 text-center">
               <h1 className="text-slanted text-capitalize">
-                search for recipe with{' '}
-                <strong className="text-danger">Food2Fork</strong>
+                search for recipe with{" "}
+                <strong className="text-danger">CookWithRupinder</strong>
               </h1>
               <form className="mt-4" onSubmit={handleSubmit}>
                 <label htmlFor="search" className="text-capitalize">
-                  type recipes seperated by comma
+                  type recipes separated by comma
                 </label>
                 <div className="input-group">
                   <input
